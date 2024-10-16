34ba7a9ea751fc64ac2fafeff4b58632dff7d5be
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-10-17 02:32:23 +0300

    Обновлены сообщения если доски не найдены

--- frontend/app/(dashboard)/[UserID]/_components/EmptySearch.tsx
+++ frontend/app/(dashboard)/[UserID]/_components/EmptySearch.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-
 export const EmptySearch = () => {
     return (
-        <div className="empty-search">
-            По вашему запросу ничего не найдено.
+        <div className="h-full flex flex-col items-center justify-center">
+            <h2 className="text-2xl font-semibold mt-6">
+                No results found!
+            </h2>
+
+            <p className="text-muted-foreground text-sm mt-2">
+                Try searching for something else!
+            </p>
         </div>
     );
 };
