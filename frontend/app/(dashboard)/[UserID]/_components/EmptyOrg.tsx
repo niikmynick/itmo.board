bfeda0bedf6c4d5412e77263573adc172a4e6044
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-10-17 02:12:23 +0300

    Добавлено предложение создать организацию

--- frontend/app/(dashboard)/[UserID]/_components/EmptyOrg.tsx
+++ frontend/app/(dashboard)/[UserID]/_components/EmptyOrg.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import Image from "next/image";
+import { CreateOrganization } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogTrigger
+} from "@/components/ui/dialog"
 
 export const EmptyOrg = () => {
     return (
-        <div className="empty-org">
-            В этой организации пока нет досок.
+        <div className="h-full flex flex-col items-center justify-center">
+            <h2 className="text-2xl font-semibold mt-6">
+                Welcome to itmo.board
+            </h2>
+            <p className="text-muted-foreground text-sm mt-2">
+                Create an organization to get started!
+            </p>
+
+            <div className="mt-6 ">
+                <Dialog>
+                    <DialogTrigger>
+                        <Button size="lg">
+                            Create an Organization!
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+                        <CreateOrganization/>
+                    </DialogContent>
+                </Dialog>
+            </div>
         </div>
     );
 };
