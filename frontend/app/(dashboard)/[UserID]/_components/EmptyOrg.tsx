74d03b39a2238df8a7e0be5a3d9b104cd7ad5997
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-10-17 01:52:54 +0300

    Созданы EmptySearch и EmptyOrg

---
+++ frontend/app/(dashboard)/[UserID]/_components/EmptyOrg.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const EmptyOrg = () => {
+    return (
+        <div className="empty-org">
+            В этой организации пока нет досок.
+        </div>
+    );
+};
---
+++ frontend/app/(dashboard)/[UserID]/_components/EmptySearch.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const EmptySearch = () => {
+    return (
+        <div className="empty-search">
+            По вашему запросу ничего не найдено.
+        </div>
+    );
+};
