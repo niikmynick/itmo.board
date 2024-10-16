1760d1ad37255505bfaea083158f69d401a85944
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-10-17 01:13:21 +0300

    Добавлены действия с доской

--- frontend/app/(dashboard)/[UserID]/_components/board-card/Index.tsx
+++ frontend/app/(dashboard)/[UserID]/_components/board-card/Index.tsx
@@ -1,16 +1,93 @@
-import React from 'react';
+"use client"
+
+import { clerkClient } from "@clerk/nextjs";
+import { MoreHorizontal } from "lucide-react";
+import { formatDistanceToNow } from "date-fns";
+
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { Overlay } from "./overlay";
+import { Footer } from "./footer";
+import { Actions } from "@/components/action";
+import { useEffect, useState } from "react";
+import { useParams, usePathname, useRouter } from "next/navigation";
 
 interface BoardCardProps {
     id: string;
     title: string;
+    authorId: string;
+    createdAt: Date;
+    imageUrl: string;
+    orgId: string;
 }
 
-export const BoardCard = ({ id, title }: BoardCardProps) => {
+export const BoardCard = ({
+                              id,
+                              title,
+                              authorId,
+                              createdAt,
+                              orgId,
+                          }: BoardCardProps) => {
+    const router = useRouter();
+    const params = useParams();
+    const [authorLabel, setAuthorLabel] = useState(params.UserID === authorId ? "You" : "Another");
+    const [loading, setLoading] = useState(false);
+    const pathname = usePathname();
+
+    useEffect(() => {
+        const getFirstName = async(userID: string) => {
+            const user = await clerkClient.users?.getUser(userID);
+            setAuthorLabel(userID === authorId ? "You" : (user?.firstName|| "Teammate"))
+        }
+        getFirstName(params.UserID as string);
+    });
+
+    const createdAtLabel = formatDistanceToNow(new Date(createdAt), {
+        addSuffix: true,
+    })
+
+    const onClick = () => {
+        try {
+            setLoading(true);
+            router.push(`boards/${id}`);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
-        <div className="group border rounded-lg flex flex-col justify-between overflow-hidden relative">
-            <div className="bg-white p-3">
-                <p className="text-[13px] truncate">{title}</p>
+        <div className="group aspect-[100/127] border rounded-lg flex cursor-pointer
+            flex-col justify-between overflow-hidden relative" onClick={onClick}>
+            <div className="relative flex-1 bg-white">
+                <Overlay/>
+                <Actions
+                    id={id}
+                    title={title}
+                    side="right"
+                >
+                    <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+                        <MoreHorizontal
+                            className="text-white opacity-75 hover:opacity-100 transition-opacity"
+                        />
+                    </button>
+                </Actions>
             </div>
+            <Footer
+                title={title}
+                authorLabel={authorLabel}
+                createdAtLabel={createdAtLabel}
+                disabled={loading}
+            />
+        </div>
+    );
+};
+
+BoardCard.Skeleton = function BoardCardSkeleton() {
+    return (
+        <div className="aspect-[100/127] rounded-lg overflow-hidden">
+            <Skeleton className="h-full w-full"/>
         </div>
     );
 };
