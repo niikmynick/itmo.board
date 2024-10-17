9ccb8d9ac1056db5c083f1d8d9210569ca899d2c
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-10-17 05:34:12 +0300

    Добавлены основные элементы диалога

---
+++ frontend/components/ui/Dialog.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import * as React from "react";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { cn } from "@/lib/utils";
+
+const Dialog = DialogPrimitive.Root;
+const DialogTrigger = DialogPrimitive.Trigger;
+const DialogPortal = DialogPrimitive.Portal;
+const DialogClose = DialogPrimitive.Close;
+
+export { Dialog, DialogTrigger, DialogPortal, DialogClose };
