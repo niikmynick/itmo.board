3b24b23b9880646a3c583ba12034716a8e5b0064
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-10-17 04:45:32 +0300

    Обновлена кнопка приглашения

--- frontend/app/(dashboard)/[UserID]/_components/InviteButton.tsx
+++ frontend/app/(dashboard)/[UserID]/_components/InviteButton.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { UserPlus } from "lucide-react";
+import { Plus } from "lucide-react";
+import { OrganizationProfile } from "@clerk/nextjs";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogTrigger
+} from "@/components/ui/dialog";
+
+import { Button } from "@/components/ui/button";
 
 export const InviteButton = () => {
     return (
-        <button className="invite-button flex items-center">
-            <UserPlus className="icon mr-2" />
-            Пригласить участников
-        </button>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline">
+                    <Plus className="h-4 w-4 mr-2"/>
+                    Invite Members
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="p-0 bg-transparent border-none max-w-[880px]">
+                <OrganizationProfile/>
+            </DialogContent>
+        </Dialog>
     );
 };
