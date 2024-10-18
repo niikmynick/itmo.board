f151db7e55ea9b397be318c607752cec9aa08b06
Vera Kasianenko <112972833+candygoose@users.noreply.github.com>
2024-10-18 19:59:34 +0300

    Обновление e2e

--- frontend/e2e/Index.e2e.ts
+++ frontend/e2e/Index.e2e.ts
@@ -1,22 +1,24 @@
 import { test, expect } from '@playwright/test';
 
 test('should log in and access page', async ({ page }) => {
+    console.log('Environment Variables:', JSON.stringify(process.env, null, 2));
+
+    const username = process.env.TEST_USERNAME;
+    const password = process.env.TEST_PASSWORD;
+
+    if (!username || !password) {
+        throw new Error(
+            'Environment variables TEST_USERNAME and TEST_PASSWORD must be set',
+        );
+    }
+
     await page.goto('http://localhost:3000/');
     console.log('Initial page content:', await page.content());
 
-    test.setTimeout(60000);
+    await page.fill('input[name="identifier"]', username);
+    await page.fill('input[name="password"]', password);
 
-    await page.goto('http://localhost:3000/');
-
-    console.log('Initial page content:', await page.content());
-    test.setTimeout(60000);
-    console.log('Initial page content:', await page.content());
-
-    await page.fill('input[name="identifier"]', 'testuser08642testuser@gmail.com');
-    await page.fill('input[name="password"]', '1es1password');
     await page.click('button[data-localization-key="formButtonPrimary"]');
-
-    await expect(page.locator('h2')).toContainText('Welcome to itmo.board');
 
     await expect(page.locator('h2')).toContainText('Welcome to itmo.board');
 });
