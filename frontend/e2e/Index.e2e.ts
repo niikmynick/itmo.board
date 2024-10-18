341e2bdb1c0fd5e56e605f9ffe2e097e455d972c
Vera Kasianenko <112972833+candygoose@users.noreply.github.com>
2024-10-18 19:54:34 +0300

    Обновление e2e

--- frontend/e2e/Index.e2e.ts
+++ frontend/e2e/Index.e2e.ts
@@ -2,13 +2,21 @@ import { test, expect } from '@playwright/test';
 
 test('should log in and access page', async ({ page }) => {
     await page.goto('http://localhost:3000/');
+    console.log('Initial page content:', await page.content());
 
-    await page.fill(
-        'input[name="identifier"]',
-        'testuser08642testuser@gmail.com',
-    );
+    test.setTimeout(60000);
+
+    await page.goto('http://localhost:3000/');
+
+    console.log('Initial page content:', await page.content());
+    test.setTimeout(60000);
+    console.log('Initial page content:', await page.content());
+
+    await page.fill('input[name="identifier"]', 'testuser08642testuser@gmail.com');
     await page.fill('input[name="password"]', '1es1password');
     await page.click('button[data-localization-key="formButtonPrimary"]');
+
+    await expect(page.locator('h2')).toContainText('Welcome to itmo.board');
 
     await expect(page.locator('h2')).toContainText('Welcome to itmo.board');
 });
