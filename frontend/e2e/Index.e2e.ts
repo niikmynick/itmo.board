2a900b1b6245164492b32581f97f19f8b37e894a
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-10-18 18:45:34 +0300

    Добавлено e2e

---
+++ frontend/e2e/Index.e2e.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('should log in and access page', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+
+    await page.fill('input[name="identifier"]', 'testuser08642testuser@gmail.com');
+    await page.fill('input[name="password"]', '1es1password');
+    await page.click('button[data-localization-key="formButtonPrimary"]');
+
+    await expect(page.locator('h2')).toContainText('Welcome to itmo.board');
+});
