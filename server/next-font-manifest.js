906356f17ca6910c01be616b38bde0bbb08e00b6
niikmynick <niikmynick@users.noreply.github.com>
2024-11-20 19:10:38 +0000

    deploy: 7375cb2451ea7a815d95f5f5a3927428607528a7

--- server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/runner/work/itmo.board/itmo.board/frontend/app/(dashboard)/[UserID]/layout":["static/media/0484562807a97172-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
